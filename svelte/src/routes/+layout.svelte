cb99b71abdda3444ed306ea0ac926a91398bbe5e
Leon Steinmeier <l.steinmeier@hzdr.de>
2025-01-06 12:56:07 +0100

    Activate SvelteKit's static site generation.

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
+	export const prerender = true;
 </script>
 
 <ParaglideJS {i18n}>
