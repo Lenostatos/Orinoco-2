d541554bdce04d13ba5e231f1cd4a2493c0214fe
Leon Steinmeier <l.steinmeier@hzdr.de>
2025-01-11 21:01:17 +0100

    Update edge with button to svelte 5 and tailwindcss.

--- svelte/src/lib/components/EdgeWButton.svelte
+++ svelte/src/lib/components/EdgeWButton.svelte
@@ -20,18 +20,6 @@
 		...restProps
 	}: EdgeProps = $props();
 
-	// type $$Props = EdgeProps;
-
-	// export let id: $$Props['id'];
-	// export let sourceX: $$Props['sourceX'];
-	// export let sourceY: $$Props['sourceY'];
-	// export let sourcePosition: $$Props['sourcePosition'];
-	// export let targetX: $$Props['targetX'];
-	// export let targetY: $$Props['targetY'];
-	// export let targetPosition: $$Props['targetPosition'];
-	// export let markerEnd: $$Props['markerEnd'] = undefined;
-	// export let style: $$Props['style'] = undefined;
-
 	const [edgePath, labelX, labelY] = $derived(
 		getBezierPath({
 			sourceX,
@@ -43,15 +31,6 @@
 		})
 	);
 
-	// $: [edgePath, labelX, labelY] = getBezierPath({
-	// 	sourceX,
-	// 	sourceY,
-	// 	sourcePosition,
-	// 	targetX,
-	// 	targetY,
-	// 	targetPosition
-	// });
-
 	const edges = useEdges();
 
 	const onEdgeClick = () => edges.update((eds) => eds.filter((edge) => edge.id !== id));
@@ -60,34 +39,17 @@
 <BaseEdge path={edgePath} {markerEnd} {style} />
 <EdgeLabelRenderer>
 	<div
-		class="edgeButtonContainer nodrag nopan"
+		class="absolute text-xs pointer-events-auto nodrag nopan"
 		style:transform="translate(-50%, -50%) translate({labelX}px,{labelY}px)"
 	>
-		<button class="edgeButton" onclick={onEdgeClick}> × </button>
+		<button
+			class="w-5 h-5, bg-gray-100 border border-white cursor-pointer rounded-[50%] text-xs hover:bg-gray-300"
+			onclick={onEdgeClick}
+		>
+			×
+		</button>
 	</div>
 </EdgeLabelRenderer>
 
 <style>
-	.edgeButtonContainer {
-		position: absolute;
-		font-size: 12pt;
-		/* everything inside EdgeLabelRenderer has no pointer events by default */
-		/* if you have an interactive element, set pointer-events: all */
-		pointer-events: all;
-	}
-
-	.edgeButton {
-		width: 20px;
-		height: 20px;
-		background: #eee;
-		border: 1px solid #fff;
-		cursor: pointer;
-		border-radius: 50%;
-		font-size: 12px;
-		line-height: 1;
-	}
-
-	.edgeButton:hover {
-		box-shadow: 0 0 6px 2px rgba(0, 0, 0, 0.08);
-	}
 </style>
